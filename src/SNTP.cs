5129e256c71025459ec4b1f82563b2cf7be88213
utak3r <piotr.borys@gmail.com>
2023-08-19 20:45:35 +0200

    SNTP client class added

--- src/SNTP.cs
+++ src/SNTP.cs
@@ -1,39 +1,155 @@
+using System.Diagnostics;
 using System.Windows.Forms;
+using System.Runtime.InteropServices;
 
 namespace iTime;
 
 public class SNTP : Form
 {
-    private System.Windows.Forms.Label lblSNTPTime;
-    private System.Windows.Forms.Button btnOk;
-    private SNTPClient sntpClient = null;
+        private System.Windows.Forms.Label? lblDateTimeLabel;
+        private System.Windows.Forms.Label? lblDateTimeValueDate;
+        private System.Windows.Forms.Label? lblDateTimeValueTime;
+        private System.Windows.Forms.Label? lblShiftLabel;
+        private System.Windows.Forms.Label? lblShiftValue;
+        private System.Windows.Forms.Button? btnClose;
+        private System.Windows.Forms.Button? btnSetTime;
+        
+        private SNTPClient? sntpClient = null;
+        private TimeSpan sntpToLocalShift = new TimeSpan();
 
     public SNTP()
     {
-        this.lblSNTPTime = new System.Windows.Forms.Label();
-        this.btnOk = new System.Windows.Forms.Button();
-        this.SuspendLayout();
-
-        this.lblSNTPTime.AutoSize =  true;
-        this.lblSNTPTime.Text =  "iTime 3.0.1";
-        this.lblSNTPTime.Location = new System.Drawing.Point(84,24);
-        this.lblSNTPTime.Size = new System.Drawing.Size(63,15);
-        this.lblSNTPTime.TabIndex = 1;
-
-        this.btnOk.Text =  "&Ok";
-        this.btnOk.Location = new System.Drawing.Point(76,84);
-        this.btnOk.TabIndex = 2;
-        this.btnOk.Click += new System.EventHandler(ButtonOkClicked);
-
-        this.Controls.AddRange(new Control[]{lblSNTPTime, btnOk});
-        this.ResumeLayout(false);
-
-        sntpClient = new SNTPClient();
+        InitializeComponent();
+        GetTimeFromSNTP();
     }
 
-    public void ButtonOkClicked(object sender, EventArgs e)
+    private void InitializeComponent()
     {
-        Close();
+        this.lblDateTimeLabel = new System.Windows.Forms.Label();
+        this.lblDateTimeValueDate = new System.Windows.Forms.Label();
+        this.lblDateTimeValueTime = new System.Windows.Forms.Label();
+        this.lblShiftLabel = new System.Windows.Forms.Label();
+        this.lblShiftValue = new System.Windows.Forms.Label();
+        this.btnClose = new System.Windows.Forms.Button();
+        this.btnSetTime = new System.Windows.Forms.Button();
+        this.SuspendLayout();
+        //
+        this.MaximizeBox =  false;
+        this.MinimizeBox =  false;
+        this.Size = new System.Drawing.Size(436,180);
+        this.Text =  "Pobieranie czasu z serwera NTP";
+        this.Icon = new Icon("satelitte.ico");
+        //
+        this.btnClose.Text =  "Zamknij okno";
+        this.btnClose.Location = new System.Drawing.Point(276,24);
+        this.btnClose.Size = new System.Drawing.Size(116,23);
+        this.btnClose.TabIndex = 0;
+        this.btnClose.Click += (sender, e) => { Close(); };
+        //
+        this.btnSetTime.Text =  "Ustaw czas";
+        this.btnSetTime.Location = new System.Drawing.Point(276,60);
+        this.btnSetTime.Size = new System.Drawing.Size(116,23);
+        this.btnSetTime.TabIndex = 1;
+        this.btnSetTime.Click += (sender, e) => { FixSystemTime(); };
+        //
+        this.lblDateTimeLabel.AutoSize =  true;
+        this.lblDateTimeLabel.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+        this.lblDateTimeLabel.Text =  "Data i godzina:";
+        this.lblDateTimeLabel.Location = new System.Drawing.Point(20,24);
+        this.lblDateTimeLabel.Size = new System.Drawing.Size(85,15);
+        //
+        this.lblDateTimeValueDate.AutoSize =  true;
+        this.lblDateTimeValueDate.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+        this.lblDateTimeValueDate.Text =  "28 październik 2023";
+        this.lblDateTimeValueDate.Location = new System.Drawing.Point(124,24);
+        this.lblDateTimeValueDate.Size = new System.Drawing.Size(108,15);
+        //
+        this.lblDateTimeValueTime.AutoSize =  true;
+        this.lblDateTimeValueTime.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+        this.lblDateTimeValueTime.Text =  "23:58:09.458";
+        this.lblDateTimeValueTime.Location = new System.Drawing.Point(124,48);
+        this.lblDateTimeValueTime.Size = new System.Drawing.Size(70,15);
+        //
+        this.lblShiftLabel.AutoSize =  true;
+        this.lblShiftLabel.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+        this.lblShiftLabel.Text =  "Przesunięcie:";
+        this.lblShiftLabel.Location = new System.Drawing.Point(20,80);
+        this.lblShiftLabel.Size = new System.Drawing.Size(75,15);
+        //
+        this.lblShiftValue.AutoSize =  true;
+        this.lblShiftValue.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+        this.lblShiftValue.Text =  "0";
+        this.lblShiftValue.Location = new System.Drawing.Point(120,80);
+        this.lblShiftValue.Size = new System.Drawing.Size(51,15);
+
+        this.Controls.AddRange(new Control[]
+            {
+            btnClose, btnSetTime, 
+            lblDateTimeLabel, lblDateTimeValueDate, lblDateTimeValueTime, 
+            lblShiftLabel, lblShiftValue
+            });
+        this.ResumeLayout(false);
+    }
+
+    private void SetDateTimeLabels(DateTime dateTime)
+    {
+        if (this.lblDateTimeValueDate != null && this.lblDateTimeValueTime != null)
+        {
+            this.lblDateTimeValueDate.Text = dateTime.ToString("dddd, dd MMMM yyyy");
+            this.lblDateTimeValueTime.Text = dateTime.ToString("HH:mm:ss.fff");
+        }
+    }
+
+    private void SetShiftLabel(TimeSpan shift)
+    {
+        if (this.lblShiftValue != null)
+        {
+            this.lblShiftValue.Text = Math.Floor(shift.TotalSeconds) + "." + Math.Abs(shift.Milliseconds) + " s.";
+        }
+    }
+
+    private void GetTimeFromSNTP()
+    {
+        DateTime localDateTime = DateTime.Now;
+        SetDateTimeLabels(localDateTime);
+
+        sntpClient = new SNTPClient();
+        sntpClient.Connect("ntp0.fau.de", 5000);
+        sntpToLocalShift = sntpClient.ReceiveTimestamp - localDateTime;
+
+        SetDateTimeLabels(sntpClient.ReceiveTimestamp);
+        SetShiftLabel(sntpToLocalShift);
+    }
+
+    [StructLayout(LayoutKind.Sequential)]
+    public struct SYSTEMTIME {
+        public short wYear;
+        public short wMonth;
+        public short wDayOfWeek;
+        public short wDay;
+        public short wHour;
+        public short wMinute;
+        public short wSecond;
+        public short wMilliseconds;
+    }
+
+    [DllImport("kernel32.dll", EntryPoint = "SetLocalTime", SetLastError = true)]
+    public extern static bool SetLocalTime(ref SYSTEMTIME sysDate);
+
+    public void FixSystemTime()
+    {
+        DateTime localDateTime = DateTime.Now;
+        DateTime correctedTime = localDateTime + sntpToLocalShift;
+
+        SYSTEMTIME st = new SYSTEMTIME();
+        st.wYear = Convert.ToInt16(correctedTime.Year);
+        st.wMonth = Convert.ToInt16(correctedTime.Month);
+        st.wDay = Convert.ToInt16(correctedTime.Day);
+        st.wHour = Convert.ToInt16(correctedTime.Hour);
+        st.wMinute = Convert.ToInt16(correctedTime.Minute);
+        st.wSecond = Convert.ToInt16(correctedTime.Second);
+        st.wMilliseconds = Convert.ToInt16(correctedTime.Millisecond);
+        SetLocalTime(ref st);
     }
 
 
---
+++ src/SNTPClient.cs
@@ -0,0 +1,417 @@
+using System.Net;
+using System.Net.Sockets;
+using System.Threading;
+
+namespace iTime;
+
+public class SNTPClient
+{
+    private const byte SNTPDataLength = 48;
+    private readonly byte[] SNTPData = new byte[SNTPDataLength];
+    private const byte offReferenceID = 12;
+    private const byte offReferenceTimestamp = 16;
+    private const byte offOriginateTimestamp = 24;
+    private const byte offReceiveTimestamp = 32;
+    private const byte offTransmitTimestamp = 40;
+
+    public enum SNTPLeapIndicator
+    {
+        NoWarning,		// 0 - No warning
+        LastMinute61,	// 1 - Last minute has 61 seconds
+        LastMinute59,	// 2 - Last minute has 59 seconds
+        Alarm			// 3 - Alarm condition (clock not synchronized)
+    }
+
+    public enum SNTPMode
+    {
+        SymmetricActive,	// 1 - Symmetric active
+        SymmetricPassive,	// 2 - Symmetric pasive
+        Client,				// 3 - Client
+        Server,				// 4 - Server
+        Broadcast,			// 5 - Broadcast
+        Unknown				// 0, 6, 7 - Reserved
+    }
+
+    public enum SNTPStratum
+    {
+        Unspecified,			// 0 - unspecified or unavailable
+        PrimaryReference,		// 1 - primary reference (e.g. radio-clock)
+        SecondaryReference,		// 2-15 - secondary reference (via NTP or SNTP)
+        Reserved				// 16-255 - reserved
+    }
+
+    /// Warns of an impending leap second to be inserted/deleted in the last
+    /// minute of the current day. (See the SNTPLeapIndicator enum)
+    public SNTPLeapIndicator LeapIndicator
+    {
+        get
+        {
+            // Isolate the two most significant bits
+            byte val = (byte)(SNTPData[0] >> 6);
+            switch (val)
+            {
+                case 0: return SNTPLeapIndicator.NoWarning;
+                case 1: return SNTPLeapIndicator.LastMinute61;
+                case 2: return SNTPLeapIndicator.LastMinute59;
+                case 3: goto default;
+                default:
+                    return SNTPLeapIndicator.Alarm;
+            }
+        }
+    }
+
+    /// Version number of the protocol (3 or 4).
+    public byte VersionNumber
+    {
+        get
+        {
+            // Isolate bits 3 - 5
+            byte val = (byte)((SNTPData[0] & 0x38) >> 3);
+            return val;
+        }
+    }
+
+    /// Returns mode. (See the SNTPMode enum)
+    public SNTPMode Mode
+    {
+        get
+        {
+            // Isolate bits 0 - 3
+            byte val = (byte)(SNTPData[0] & 0x7);
+            switch (val)
+            {
+                case 0: goto default;
+                case 6: goto default;
+                case 7: goto default;
+                default:
+                    return SNTPMode.Unknown;
+                case 1:
+                    return SNTPMode.SymmetricActive;
+                case 2:
+                    return SNTPMode.SymmetricPassive;
+                case 3:
+                    return SNTPMode.Client;
+                case 4:
+                    return SNTPMode.Server;
+                case 5:
+                    return SNTPMode.Broadcast;
+            }
+        }
+    }
+
+    /// Stratum of the clock. (See the SNTPStratum enum)
+    public SNTPStratum Stratum
+    {
+        get
+        {
+            byte val = (byte)SNTPData[1];
+            if (val == 0) return SNTPStratum.Unspecified;
+            else
+                if (val == 1) return SNTPStratum.PrimaryReference;
+                else
+                    if (val <= 15) return SNTPStratum.SecondaryReference;
+                    else
+                        return SNTPStratum.Reserved;
+        }
+    }
+
+    /// Maximum interval (seconds) between successive messages
+    public uint PollInterval
+    {
+        get
+        {
+            // Thanks to Jim Hollenhorst <hollenho@attbi.com>
+            return (uint)(Math.Pow(2, (sbyte)SNTPData[2]));
+        }
+    }
+
+    /// Precision (in seconds) of the clock
+    public double Precision
+    {
+        get
+        {
+            // Thanks to Jim Hollenhorst <hollenho@attbi.com>
+            return (Math.Pow(2, (sbyte)SNTPData[3]));
+        }
+    }
+
+    /// Round trip time (in milliseconds) to the primary reference source.
+    public double RootDelay
+    {
+        get
+        {
+            int temp = 0;
+            temp = 256 * (256 * (256 * SNTPData[4] + SNTPData[5]) + SNTPData[6]) + SNTPData[7];
+            return 1000 * (((double)temp) / 0x10000);
+        }
+    }
+
+    /// Nominal error (in milliseconds) relative to the primary reference source.
+    public double RootDispersion
+    {
+        get
+        {
+            int temp = 0;
+            temp = 256 * (256 * (256 * SNTPData[8] + SNTPData[9]) + SNTPData[10]) + SNTPData[11];
+            return 1000 * (((double)temp) / 0x10000);
+        }
+    }
+
+    /// Reference identifier (either a 4 character string or an IP address)
+    public string ReferenceID
+    {
+        get
+        {
+            string val = "";
+            switch (Stratum)
+            {
+                case SNTPStratum.Unspecified:
+                    goto case SNTPStratum.PrimaryReference;
+                case SNTPStratum.PrimaryReference:
+                    val += (char)SNTPData[offReferenceID + 0];
+                    val += (char)SNTPData[offReferenceID + 1];
+                    val += (char)SNTPData[offReferenceID + 2];
+                    val += (char)SNTPData[offReferenceID + 3];
+                    break;
+                case SNTPStratum.SecondaryReference:
+                    switch (VersionNumber)
+                    {
+                        case 3:	// Version 3, Reference ID is an IPv4 address
+                            string Address = SNTPData[offReferenceID + 0].ToString() + "." +
+                                                SNTPData[offReferenceID + 1].ToString() + "." +
+                                                SNTPData[offReferenceID + 2].ToString() + "." +
+                                                SNTPData[offReferenceID + 3].ToString();
+                            try
+                            {
+                                IPHostEntry Host = Dns.GetHostEntry(Address);
+                                val = Host.HostName + " (" + Address + ")";
+                            }
+                            catch (Exception)
+                            {
+                                val = "N/A";
+                            }
+                            break;
+                        case 4: // Version 4, Reference ID is the timestamp of last update
+                            DateTime time = ComputeDate(GetMilliSeconds(offReferenceID));
+                            // Take care of the time zone                                
+                            TimeSpan offspan = TimeZoneInfo.Local.GetUtcOffset(DateTime.UtcNow);
+                            val = (time + offspan).ToString();
+                            break;
+                        default:
+                            val = "N/A";
+                            break;
+                    }
+                    break;
+            }
+
+            return val;
+        }
+    }
+
+    /// The time at which the clock was last set or corrected
+    public DateTime ReferenceTimestamp
+    {
+        get
+        {
+            DateTime time = ComputeDate(GetMilliSeconds(offReferenceTimestamp));
+            // Take care of the time zone
+            TimeSpan offspan = TimeZoneInfo.Local.GetUtcOffset(DateTime.UtcNow);
+            return time + offspan;
+        }
+    }
+
+    /// The time (T1) at which the request departed the client for the server
+    public DateTime OriginateTimestamp
+    {
+        get
+        {
+            return ComputeDate(GetMilliSeconds(offOriginateTimestamp));
+        }
+    }
+
+    /// The time (T2) at which the request arrived at the server
+    public DateTime ReceiveTimestamp
+    {
+        get
+        {
+            DateTime time = ComputeDate(GetMilliSeconds(offReceiveTimestamp));
+            // Take care of the time zone
+            TimeSpan offspan = TimeZoneInfo.Local.GetUtcOffset(DateTime.UtcNow);
+            return time + offspan;
+        }
+    }
+
+    /// The time (T3) at which the reply departed the server for client
+    public DateTime TransmitTimestamp
+    {
+        get
+        {
+            DateTime time = ComputeDate(GetMilliSeconds(offTransmitTimestamp));
+            // Take care of the time zone
+            TimeSpan offspan = TimeZoneInfo.Local.GetUtcOffset(DateTime.UtcNow);
+            return time + offspan;
+        }
+        set
+        {
+            SetDate(offTransmitTimestamp, value);
+        }
+    }
+
+    /// Destination Timestamp (T4)
+    public DateTime DestinationTimestamp;
+
+    /// The time (in milliseconds) between the departure of request and arrival of reply 
+    public double RoundTripDelay
+    {
+        get
+        {
+            TimeSpan span = (DestinationTimestamp - OriginateTimestamp) - (ReceiveTimestamp - TransmitTimestamp);
+            return span.TotalMilliseconds;
+        }
+    }
+
+    /// The offset (in milliseconds) of the local clock relative to the primary reference source
+    public double LocalClockOffset
+    {
+        get
+        {
+            TimeSpan span = (ReceiveTimestamp - OriginateTimestamp) + (TransmitTimestamp - DestinationTimestamp);
+            return (span.TotalMilliseconds / 2);
+        }
+    }
+
+    /// Compute date, given the number of milliseconds since January 1, 1900
+    private DateTime ComputeDate(ulong milliseconds)
+    {
+        TimeSpan span = TimeSpan.FromMilliseconds((double)milliseconds);
+        DateTime time = new DateTime(1900, 1, 1);
+        time += span;
+        return time;
+    }
+
+    /// Compute the number of milliseconds, given the offset of a 8-byte array
+    private ulong GetMilliSeconds(byte offset)
+    {
+        ulong intpart = 0, fractpart = 0;
+
+        for (int i = 0; i <= 3; i++)
+        {
+            intpart = 256 * intpart + SNTPData[offset + i];
+        }
+        for (int i = 4; i <= 7; i++)
+        {
+            fractpart = 256 * fractpart + SNTPData[offset + i];
+        }
+        ulong milliseconds = intpart * 1000 + (fractpart * 1000) / 0x100000000L;
+        return milliseconds;
+    }
+
+    /// Set the date part of the SNTP data
+    /// <param name="offset">Offset at which the date part of the SNTP data is</param>
+    /// <param name="date">The date</param>
+    private void SetDate(byte offset, DateTime date)
+    {
+        ulong intpart = 0, fractpart = 0;
+        DateTime StartOfCentury = new DateTime(1900, 1, 1, 0, 0, 0);	// January 1, 1900 12:00 AM
+
+        ulong milliseconds = (ulong)(date - StartOfCentury).TotalMilliseconds;
+        intpart = milliseconds / 1000;
+        fractpart = ((milliseconds % 1000) * 0x100000000L) / 1000;
+
+        ulong temp = intpart;
+        for (int i = 3; i >= 0; i--)
+        {
+            SNTPData[offset + i] = (byte)(temp % 256);
+            temp /= 256;
+        }
+
+        temp = fractpart;
+        for (int i = 7; i >= 4; i--)
+        {
+            SNTPData[offset + i] = (byte)(temp % 256);
+            temp /= 256;
+        }
+    }
+
+    /// Returns true if received data is valid and if comes from a NTP-compliant time server.
+    private bool IsResponseValid()
+    {
+        if (SNTPData.Length < SNTPDataLength || Mode != SNTPMode.Server)
+        {
+            return false;
+        }
+        else
+        {
+            return true;
+        }
+    }
+
+
+    private DateTime GetCurrentTime() => DateTime.Now;
+
+    public SNTPClient()
+    {
+        SNTPData[0] = 0x1B;
+        for (int i = 1; i < 48; i++)
+        {
+            SNTPData[i] = 0;
+        }
+        TransmitTimestamp = GetCurrentTime();
+    }
+
+    public void Connect(string Host, int TimeOut)
+    {
+        try
+        {
+            IPEndPoint listenEP = new IPEndPoint(IPAddress.Any, 123);
+            Socket sendSocket = new Socket(AddressFamily.InterNetwork, SocketType.Dgram, ProtocolType.Udp);
+            IPHostEntry hostEntry = Dns.GetHostEntry(Host);
+            IPEndPoint sendEP = new IPEndPoint(hostEntry.AddressList[0], 123);
+            EndPoint epSendEP = (EndPoint)sendEP;
+
+            int messageLength = 0;
+            try
+            {
+                sendSocket.Bind(listenEP);
+
+                bool messageReceived = false;
+                int elapsedTime = 0;
+
+                while (!messageReceived && (elapsedTime < TimeOut))
+                {
+                    sendSocket.SendTo(SNTPData, SNTPData.Length, SocketFlags.None, sendEP);
+                    if (sendSocket.Available > 0)
+                    {
+                        messageLength = sendSocket.ReceiveFrom(SNTPData, ref epSendEP);
+                        if (!IsResponseValid())
+                        {
+                            throw new Exception($"Host sent an invalid response.");
+                        }
+                        messageReceived = true;
+                        break;
+                    }
+                    Thread.Sleep(500);
+                    elapsedTime += 500;
+                }
+                if (!messageReceived)
+                {
+                    throw new TimeoutException($"Host did not respond.");
+                }
+            }
+            catch (SocketException ex)
+            {
+                throw new Exception(ex.Message);
+            }
+            finally
+            {
+                sendSocket.Close();
+            }
+        }
+        catch (SocketException e)
+        {
+            throw new Exception(e.Message);
+        }
+    }
+
+
+}
